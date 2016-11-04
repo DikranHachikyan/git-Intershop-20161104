61b42e3d579ff2beada11c53bac97e8d7752555c
Dikran Hachikyan <dhachikian@expert-bg.org>
2016-11-04 13:10:47 +0200

    Get/set methods

--- FileSearchApp.java
+++ FileSearchApp.java
@@ -24,7 +24,34 @@ public class FileSearchApp {
         }
         
     }
+
     public void walkDirectory(String path){
-        
+
     }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public void setRegex(String regex) {
+        this.regex = regex;
+        this.pattern = Pattern.compile(regex);
+    }
+
+    public String getZipFileName() {
+        return zipFileName;
+    }
+
+    public void setZipFileName(String zipFileName) {
+        this.zipFileName = zipFileName;
+    }
+
 }
